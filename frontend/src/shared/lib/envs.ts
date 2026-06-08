2299774b538aa45d8f38176d17387a294ea46430
Tomas Elordi <tomaselordi3@gmail.com>
2026-06-08 17:32:31 -0300

    fix: cookies session error on safari

--- frontend/src/shared/lib/envs.ts
+++ frontend/src/shared/lib/envs.ts
@@ -1,3 +1,3 @@
 export const apiBaseUrl = process.env.API_URL;
 export const accessTokenMaxAge: number =
-  Number(process.env.ACCESS_TOKEN_MAX_AGE) ?? 60 * 60 * 24;
+  Number(process.env.ACCESS_TOKEN_MAX_AGE) || 60 * 60 * 24;
--- frontend/src/shared/lib/session.ts
+++ frontend/src/shared/lib/session.ts
@@ -11,7 +11,7 @@ const COOKIE_KEYS = {
 const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict" as const,
+  sameSite: "lax" as const,
 };
 
 export const session = {
