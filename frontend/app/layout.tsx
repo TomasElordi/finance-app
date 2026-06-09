801f6f6603c625807cd28d75f033fca076494c23
Tomas Elordi <tomaselordi3@gmail.com>
2026-06-09 12:04:58 -0300

    feat: add vercel speed insights

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -29,6 +30,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable}  font-sans h-full antialiased bg-background text-foreground dark`}
     >
       <Analytics />
+      <SpeedInsights />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
